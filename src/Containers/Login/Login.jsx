9ad47b6d0a35bdbfc41b7b05b04bb656b63b8b9a
suku60 <juanmaxp2@gmail.com>
2022-03-05 20:26:08 +0100

    Login form created.

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -4,12 +4,20 @@ import './Login.css';
 const Login = () => {
 
     return (
-       <div className="box_login">
-           login here
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo eum molestiae, illum, consequuntur esse totam odio cupiditate impedit sapiente blanditiis omnis? Delectus ad molestias, quibusdam voluptate totam illum earum voluptates!
-
-       </div>
-    )
-}
+        <div className="box_login">
+            <div className="full_form_box_login">
+                <div className="full_form_box_container" id="mid_form_box">    
+                    <div className="form_box">EMAIL
+                    </div>
+                    <div className="form_box">PASSWORD
+                    </div>
+                </div>
+                <div className="full_form_box_container">
+                    <div className="full_form_box_line" id="login_button">login</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Login;
