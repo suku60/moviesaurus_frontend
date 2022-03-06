60c2a0b15c2308f321ed15638ad2ff21076c2e86
suku60 <juanmaxp2@gmail.com>
2022-03-06 19:51:29 +0100

    Register function phase 3 completed. Starting tests with the backend.

--- src/Containers/Register/Register.jsx
+++ src/Containers/Register/Register.jsx
@@ -15,6 +15,7 @@ const Register = () => {
         birthdate: "",
         username: "",
         password: "",
+        password2: "",
         email: ""
     });
 
@@ -59,51 +60,84 @@ const Register = () => {
         setValidationMessage("");
       }
 
+      // Birthdate function: 3 different inputs with 3 fields = 2(day)/2(month)/4(year)
+      // all 3 combined make a whole date (find dataType that is useful for this)
+
+      // if(userData.birthdate !== ){
+
+      //   return (setValidationMessage("Both password fields must match"));
+
+      // }else{
+      //   setValidationMessage("");
+      // }
+
       for(let field of rawUserData){
         // error / validations
-        errorDisplay = validations[field[0],field[1]]
+        errorDisplay = validations[field[0],field[1]];
+
+        if(errorDisplay !== "valid"){
+          setValidationMessage(errorDisplay);
+          return;
+
+          // when the validations are not ok, we're not going to allow the register
+
+        };
+      };
+
+      // Build the body for the post data send.
+
+      let dataBody = {
+        name: userData.name,
+        birthdate: userData.birthdate,
+        username: userData.username,
+        password: userData.password,
+        email: userData.email
       }
 
+      try {
 
+        let dataPost = await axios.post("endpoint", dataBody)
 
+      setTimeout(() => {
+        desiredView("/login")
+      }, 1500);
 
-
+      }catch(errorDisplay) {
+      
+      }
+      
     };
 
 
-      return(
-        <div></div>
-      );
-
-    // return (
-    //     <div className="register_view">
-    //     <div className="full_form_box">
-    //       <div className="full_form_box_container">PLEASE COMPLETE THE FORM TO REGISTER</div>
-    //       <div className="full_form_box_container" id="mid_form_box"><div className="full_form_box_line">
-    //         {/* INPUTS HAVE TO BE RENAMED */}
-    //       <div className="form_box">NAME</div>
-    //       <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
-    //       </div>
-    //       <div className="full_form_box_line"><div className="form_box">BIRTHDATE</div>
-    //       <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
-    //       </div>
-    //       <div className="full_form_box_line"><div className="form_box">USERNAME</div>
-    //       <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
-    //       </div>
-    //       <div className="full_form_box_line"><div className="form_box">PASSWORD</div>
-    //       <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
-    //       </div>
-    //       <div className="full_form_box_line"><div className="form_box">EMAIL</div>
-    //       <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
-    //       </div>
-    //       </div>
+    return (
+        <div className="register_view">
+        <div className="full_form_box">
+          <div className="full_form_box_container">PLEASE COMPLETE THE FORM TO REGISTER</div>
+          <div className="full_form_box_container" id="mid_form_box"><div className="full_form_box_line">
+            {/* INPUTS HAVE TO BE RENAMED */}
+          <div className="form_box">NAME</div>
+          <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
+          </div>
+          <div className="full_form_box_line"><div className="form_box">BIRTHDATE</div>
+          <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
+          </div>
+          <div className="full_form_box_line"><div className="form_box">USERNAME</div>
+          <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
+          </div>
+          <div className="full_form_box_line"><div className="form_box">PASSWORD</div>
+          <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
+          </div>
+          <div className="full_form_box_line"><div className="form_box">EMAIL</div>
+          <input className="form_input" type="name" name="name" id="name" title="name" placeholder="Name:" autoComplete="off" /*missing somehting here*//>
+          </div>
+          </div>
           
-    //       <div className="full_form_box_container">
-    //       <div className="full_form_box_line" id="register_button">register me</div>
-    //       </div>
-    //       </div>
-    //   </div>
-    // );
+          <div className="full_form_box_container">
+          <div className="full_form_box_line" id="register_button">register me</div>
+          </div>
+          </div>
+      </div>
+    );
 };
 
 export default Register;
